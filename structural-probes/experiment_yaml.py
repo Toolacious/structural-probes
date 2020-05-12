752ceebdcece402112014c84f6df1ed9a207db6f
Sologa <b04901114@ntu.edu.tw>
2020-05-13 04:38:32 +0800

    Update experiment_yaml.py

--- structural-probes/experiment_yaml.py
+++ structural-probes/experiment_yaml.py
@@ -4,7 +4,7 @@ from argparse import ArgumentParser
 
 
 argp = ArgumentParser()
-argp.add_argument('--model_layer', default=0, help='0 ~ 12')
+argp.add_argument('--model_layer', default=0, help='0 ~ 11')
 argp.add_argument('--task_name', default=0, help='parse-distance or parse-depth')
 args = argp.parse_args()
 
@@ -16,8 +16,8 @@ file['model']['model_layer'] = int(args.model_layer)
 file['probe']['task_name'] = args.task_name
 file['probe']['params_path'] = 'predictor_{}_{}.params'.format(args.model_layer, args.task_name)
 
-file['probe']['maximum_rank'] = 256
-file['dataset']['batch_size'] = 80
+file['probe']['maximum_rank'] = 128
+file['dataset']['batch_size'] = 10
 file['probe_training']['epochs'] = 30
 
 if args.task_name == 'parse-depth':
